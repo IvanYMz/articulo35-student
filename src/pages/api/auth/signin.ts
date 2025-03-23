bf3aa484c925a5164a91d056c63dbf0880d74539
Ivan Morales <ivanymz09@gmail.com>
2025-03-23 13:30:26 -0600

    refactor: move home content to index

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -41,7 +41,7 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   }
 
   if (userRole.role === "student") {
-    return redirect("/home");
+    return redirect("/");
   } 
 
   cookies.delete("sb-access-token", { path: "/" });
